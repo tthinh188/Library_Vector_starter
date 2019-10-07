b813e05c0cde9bd3bf7441214ee8a3aaa2e4c2f9
Thinh Phan <thinh.phan.18@cnu.edu>
2019-10-07 11:28:49 -0400

    project3

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -1,5 +1,12 @@
 #include "../includes_usr/fileIO.h"
+
+#include <fstream>
+#include <string>
+#include "../includes_usr/constants.h"
+
 using namespace std;
+book bookObject;
+patron patronObject;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries in books
@@ -7,6 +14,70 @@ using namespace std;
  * */
 int loadBooks(std::vector<book> &books, const char* filename)
 {
+	books.clear();
+	std::string line;
+	fstream myfile;
+	string readLine;
+	myfile.open(filename);
+
+	if (!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+	else{
+		getline(myfile, line);
+		if(line.length() == 0) {
+			return NO_BOOKS_IN_LIBRARY;
+		}
+		else {
+			int comma = line.find(',');
+			readLine = line.substr(0,comma);
+			bookObject.book_id = std::stoi(readLine);
+			line = line.substr(comma +1);
+			comma = line.find(',');
+			readLine = line.substr(0,comma);
+			bookObject.title = readLine;
+			line = line.substr(comma +1);
+			comma = line.find(',');
+			readLine = line.substr(0,comma);
+			bookObject.author = readLine;
+			line = line.substr(comma +1);
+			comma = line.find(',');
+			readLine = line.substr(0,comma);
+			bookObject.state = IN;
+			line = line.substr(comma +1);
+			comma = line.find(',');
+			readLine= line.substr(0,comma);
+			bookObject.loaned_to_patron_id = std::stoi(readLine);
+			books.push_back(bookObject);
+
+			while (!myfile.eof()){
+				getline(myfile, line);
+				if(line.length() != 0) {
+					comma = line.find(',');
+					readLine = line.substr(0,comma);
+					bookObject.book_id = std::stoi(readLine);
+					line = line.substr(comma +1);
+					comma = line.find(',');
+					readLine = line.substr(0,comma);
+					bookObject.title = readLine;
+					line = line.substr(comma +1);
+					comma = line.find(',');
+					readLine = line.substr(0,comma);
+					bookObject.author = readLine;
+					line = line.substr(comma +1);
+					comma = line.find(',');
+					readLine = line.substr(0,comma);
+					bookObject.state = IN;
+					line = line.substr(comma +1);
+					comma = line.find(',');
+					readLine= line.substr(0,comma);
+					bookObject.loaned_to_patron_id = std::stoi(readLine);
+					books.push_back(bookObject);
+				}
+			}
+		}
+	}
+	myfile.close();
 	return SUCCESS;
 }
 
@@ -17,6 +88,39 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * */
 int saveBooks(std::vector<book> &books, const char* filename)
 {
+	book bookObject;
+	std::string line;
+	ofstream myfile;
+	myfile.open(filename);
+	if (!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+	else {
+		if (books.size() == 0) {
+			return NO_BOOKS_IN_LIBRARY;
+		}
+		else {
+			int lengOfVector = books.size();
+			for (int i = 0; i < lengOfVector; i++){
+				bookObject = books[i];
+				line = std::to_string(bookObject.book_id);
+				line = line + "," + bookObject.title;
+				line = line + "," + bookObject.author + ",";
+				if (bookObject.state == UNKNOWN){
+					line = line + "0,";
+				}
+				else if (bookObject.state == IN){
+					line = line + "1,";
+				}
+				else{
+					line = line + "2,";
+				}
+				line = line + std::to_string(bookObject.loaned_to_patron_id) + "\n";
+				myfile << line;
+			}
+		}
+	}
+	myfile.close();
 	return SUCCESS;
 }
 
@@ -27,6 +131,52 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename)
 {
+	std::string line;
+	fstream myfile;
+	string readLine;
+	patrons.clear();
+	myfile.open(filename);
+	if (!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+	else{
+		getline(myfile, line);
+		if(line.length() == 0) {
+			return NO_PATRONS_IN_LIBRARY;
+		}
+		else {
+			int comma = line.find(',');
+			readLine = line.substr(0,comma);
+			patronObject.patron_id = std::stoi(readLine);
+			line = line.substr(comma +1);
+			comma = line.find(',');
+			readLine = line.substr(0,comma);
+			patronObject.name = readLine;
+			line = line.substr(comma +1);
+			comma = line.find(',');
+			readLine = line.substr(0,comma);
+			patronObject.number_books_checked_out = std::stoi(readLine);
+			patrons.push_back(patronObject);
+			while (!myfile.eof()){
+				getline(myfile, line);
+				if(line.length() != 0) {
+					comma = line.find(',');
+					readLine = line.substr(0,comma);
+					patronObject.patron_id = std::stoi(readLine);
+					line = line.substr(comma +1);
+					comma = line.find(',');
+					readLine = line.substr(0,comma);
+					patronObject.name = readLine;
+					line = line.substr(comma +1);
+					comma = line.find(',');
+					readLine = line.substr(0,comma);
+					patronObject.number_books_checked_out = std::stoi(readLine);
+					patrons.push_back(patronObject);
+				}
+		}
+		}
+	}
+	myfile.close();
 	return SUCCESS;
 }
 
@@ -37,5 +187,29 @@ int loadPatrons(std::vector<patron> &patrons, const char* filename)
  * */
 int savePatrons(std::vector<patron> &patrons, const char* filename)
 {
+	patron patronObject;
+	std::string line;
+	ofstream myfile;
+	myfile.open(filename);
+
+	if (!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+	else {
+		if (patrons.size() == 0) {
+			return NO_PATRONS_IN_LIBRARY;
+		}
+		else{
+			int lengOfVector = patrons.size();
+			for (int i = 0; i < lengOfVector; i++){
+				patronObject = patrons[i];
+				line = std::to_string(patronObject.patron_id);
+				line = line + "," + patronObject.name + ",";
+				line = line + std::to_string(patronObject.number_books_checked_out) +"\n";
+				myfile << line;
+			}
+	myfile.close();
 	return SUCCESS;
+	}
+}
 }
--- library/library.cpp
+++ library/library.cpp
@@ -8,7 +8,8 @@
 #include "../includes_usr/datastructures.h"
 #include "../includes_usr/fileIO.h"
 using namespace std;
-
+vector<book> books;
+vector<patron> patrons;
 //NOTE: please ensure patron and book data are loaded from disk before calling the following
 //NOTE: also make sure you save patron and book data to disk any time you make a change to them
 //NOTE: for files where data is stored see constants.h BOOKFILE and PATRONFILE
@@ -18,7 +19,10 @@ using namespace std;
  * then reload them from disk 
  */
 void reloadAllData(){
-
+	books.clear();
+	patrons.clear();
+	loadBooks(books, BOOKFILE.c_str());
+	loadPatrons(patrons, PATRONFILE.c_str());
 }
 
 /* checkout a book to a patron
@@ -42,7 +46,37 @@ void reloadAllData(){
  *         TOO_MANY_OUT patron has the max number of books allowed checked out
  */
 int checkout(int bookid, int patronid){
-	return SUCCESS;
+	int patronIndex = UNINITIALIZED;
+	int bookIndex = UNINITIALIZED;
+	loadBooks(books, BOOKFILE.c_str());
+	loadPatrons(patrons, PATRONFILE.c_str());
+	for (int i = 0; i < numbPatrons(); i++){
+		if (patronid == patrons[i].patron_id){
+			patronIndex = i;
+		}
+	}
+	for (int i = 0; i < numbBooks(); i++){
+		if (bookid == books[i].book_id){
+			bookIndex = i;
+		}
+	}
+	if (patronIndex == UNINITIALIZED){
+		return PATRON_NOT_ENROLLED;
+	}
+	if (bookIndex == UNINITIALIZED || (books[bookIndex].state == OUT)) {
+		return BOOK_NOT_IN_COLLECTION;
+	}
+	if (howmanybooksdoesPatronHaveCheckedOut(patronid) >= MAX_BOOKS_ALLOWED_OUT){
+		return TOO_MANY_OUT;
+	}
+	else {
+		patrons[patronIndex].number_books_checked_out ++ ;
+		books[bookIndex].loaned_to_patron_id = patrons[patronIndex].patron_id;
+		books[bookIndex].state = OUT;
+		saveBooks(books, BOOKFILE.c_str());
+		savePatrons(patrons, PATRONFILE.c_str());
+		return SUCCESS;
+	}
 }
 
 /* check a book back in 
@@ -58,7 +92,31 @@ int checkout(int bookid, int patronid){
  * 		   BOOK_NOT_IN_COLLECTION
  */
 int checkin(int bookid){
-	return SUCCESS;
+	int patronIndex = UNINITIALIZED;
+	int bookIndex = UNINITIALIZED;
+	loadBooks(books, BOOKFILE.c_str());
+	loadPatrons(patrons, PATRONFILE.c_str());
+	for (int i = 0; i < numbBooks(); i++){
+		if (bookid == books[i].book_id){
+			bookIndex = i;
+		}
+	}
+	if (bookIndex == UNINITIALIZED) {
+		return BOOK_NOT_IN_COLLECTION;
+	}
+	else{
+		for (int i = 0; i < numbPatrons(); i++){
+			if (books[bookIndex].loaned_to_patron_id == patrons[i].patron_id){
+				patronIndex = i;
+			}
+		}
+		books[bookIndex].state = IN;
+		books[bookIndex].loaned_to_patron_id = NO_ONE;
+		patrons[patronIndex].number_books_checked_out --;
+		saveBooks(books, BOOKFILE.c_str());
+		savePatrons(patrons, PATRONFILE.c_str());
+		return SUCCESS;
+	}
 }
 
 /*
@@ -71,7 +129,22 @@ int checkin(int bookid){
  *    the patron_id of the person added
  */
 int enroll(std::string &name){
-	return 0;
+	patron patronObject;
+	loadBooks(books, BOOKFILE.c_str());
+	loadPatrons(patrons, PATRONFILE.c_str());
+	if (patrons.size() == 0){ // there is no Patron in the container.
+		patronObject.name = name;
+		patronObject.patron_id = 0;
+		patronObject.number_books_checked_out = NONE;
+	}
+	else{
+	patronObject.name = name;
+	patronObject.patron_id = patrons[patrons.size()-1].patron_id + 1;
+	patronObject.number_books_checked_out = NONE;
+	}
+	patrons.push_back(patronObject);
+	savePatrons(patrons, PATRONFILE.c_str());
+	return patronObject.patron_id;
 }
 
 /*
@@ -80,7 +153,8 @@ int enroll(std::string &name){
  * 
  */
 int numbBooks(){
-	return 0;
+	loadBooks(books, BOOKFILE.c_str());
+	return books.size();
 }
 
 /*
@@ -88,7 +162,8 @@ int numbBooks(){
  * (ie. if 3 patrons returns 3)
  */
 int numbPatrons(){
-	return 0;
+	loadPatrons(patrons, PATRONFILE.c_str());
+	return patrons.size();
 }
 
 /*the number of books patron has checked out
@@ -97,7 +172,18 @@ int numbPatrons(){
  *        or PATRON_NOT_ENROLLED         
  */
 int howmanybooksdoesPatronHaveCheckedOut(int patronid){
-	return 0;
+	int patronIndex = UNINITIALIZED;
+	for (int i = 0; i < numbPatrons(); i++){
+		if (patronid == patrons[i].patron_id){
+			patronIndex = i;
+		}
+	}
+	if (patronIndex == UNINITIALIZED){
+		return PATRON_NOT_ENROLLED;
+	}
+	else{
+		return patrons[patronIndex].number_books_checked_out;
+	}
 }
 
 /* search through patrons container to see if patronid is there
@@ -107,6 +193,7 @@ int howmanybooksdoesPatronHaveCheckedOut(int patronid){
  *         PATRON_NOT_ENROLLED no patron with this patronid
  */
 int whatIsPatronName(std::string &name,int patronid){
+
 	return SUCCESS;
 }
 
